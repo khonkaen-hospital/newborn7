078f9e61e23077d1bc3a44badd1fee4ef5ac8347
Sathit Seethaphon <dixonsatit@gmail.com>
2016-06-29 01:52:06 +0700

    เพิ่มเมนูและปรับปรุงหน้า index

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -35,9 +35,13 @@ AppAsset::register($this);
         ],
     ]);
     $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+        ['label' => 'หน้าหลัก', 'url' => ['/site/index']],
+        ['label' => 'ข้อมูล New Born', 'items' => [
+          ['label' => 'บันทึก KPI', 'url' => ['/site/about']],
+          ['label' => 'ข้อมูล New Born', 'url' => ['/site/about']],
+        ]],
+        // ['label' => 'About', 'url' => ['/site/about']],
+        // ['label' => 'Contact', 'url' => ['/site/contact']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/user/registration/register']];
